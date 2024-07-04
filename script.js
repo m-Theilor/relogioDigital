47dc48568a9ec9149796bcd6a31eff8ef76fba68
Theilor Martins <160059582+m-Theilor@users.noreply.github.com>
2024-07-04 19:48:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let horas = document.getElementById('horas');
+let minutos = document.getElementById('minutos');
+let segundos = document.getElementById('segundos');
+
+let relogio = setInterval(function time() {
+    let dateToday = new Date();
+    let hr = dateToday.getHours();
+    let min = dateToday.getMinutes();
+    let sec = dateToday.getSeconds();
+
+    if (hr < 10) {
+        hr = '0' + hr;
+    }
+    if (min < 10) {
+        min = '0' + min;
+    }
+    if (sec < 10) {
+        sec = '0' + sec;
+    }
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = sec;
+})
